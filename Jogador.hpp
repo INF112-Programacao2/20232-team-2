3716fb556b9cb7b07bbcef61c89c74961be03498
Gabriel Souza <anderson.rosario@ufv.br>
2023-12-07 16:45:12 -0300

    Alteração da visibilidade dos atributos de Jogador Criação dos métods get e isTrue para acessar os valores dos atributos

--- Jogador.hpp
+++ Jogador.hpp
@@ -1,5 +1,6 @@
 #include "Carta.hpp"
 #include <vector>
+#include <iostream>
 
 struct Mao
 {
@@ -10,9 +11,9 @@ struct Mao
 
 class Jogador
 {
-   protected:
-      char *Nome; // nome do jogador
-      Mao Mao; // mao composta por 2 cartas
+   private:
+      const std::string *Nome; // nome do jogador
+      const Mao Mao; // mao composta por 2 cartas
       std::vector<std::pair<int,int>> Fichas; // Quantas fichas de qual tipo de ficha; exemplo: 20 fichas de 10, 5 fichas de 100.
       bool Vez; // Verifica se está na vez do jogador x
       bool Ativo; // Verifica se o jogador está na rodada ou se desistiu da rodada
@@ -21,6 +22,15 @@ class Jogador
       bool Small_Blind; // Jogador é ou nao é small blind
 
    public:
+      //métodos get para acesso aos atributos de Jogador
+      const std::string get_Nome();
+      const Mao get_Mao();
+      bool isTrue_Vez();
+      bool isTrue_Ativo();
+      bool isTrue_Cobriu();
+      bool isTrue_Big_Blind();
+      bool isTrue_Small_Blind();
+
       void Check();
       void Apostar();
       void Igualar();
