85406e9dfaf72726c5bcd5ce8bcb6ef34f07bc2a
Hygor Souza <hygor.souza@ufv.br>
2023-12-10 21:12:26 -0300

    Primeiros testes praticos

--- main.cpp
+++ main.cpp
@@ -5,8 +5,36 @@
 #include "Carta.hpp"
 #include "Mao.hpp"
 
+
 int main()
 {
    Dealer ricardo;
+
+   std::system("clear");
+
+
+   //teste dealer
+
+   /*for (int i = 0; i < ricardo.get_baralho().size(); i++)
+   {
+      std::cout << ricardo.get_baralho()[i].get_Valor_Carta() << ' ' << ricardo.get_baralho()[i].get_Naipe() << "\n";
+   }
+
+   std::cout << "\n";
+
+   for (int i = 0; i < ricardo.get_mesa().size(); i++)
+   {
+      std::cout << ricardo.get_mesa()[i].get_Valor_Carta() << ' ' << ricardo.get_mesa()[i].get_Naipe() << "\n";
+   }*/
+   
+   //teste jogador
+
+   /*int a = 100;
+   Jogador C("Coshe");
+   C.exibirInfo(a);
+   C.apostar(a);
+   C.exibirInfo(a);
+   */
+
    return 0;
 }
