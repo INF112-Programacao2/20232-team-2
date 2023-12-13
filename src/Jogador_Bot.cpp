4d5ac79f0b4317eb90c63b13b61871030d6db5e3
Gabriel Souza <anderson.rosario@ufv.br>
2023-12-13 12:25:16 -0300

    Primeira implementação da sobrescrita do método apostar para a classe Jogador_Bot

--- src/Jogador_Bot.cpp
+++ src/Jogador_Bot.cpp
@@ -7,6 +7,46 @@ Jogador_Bot::Jogador_Bot(std::string _nick) : Jogador(_nick)
 
 bool Jogador_Bot::apostar(int &valorMesa) 
 {
-   return true;
+   int aposta = valorMesa - this->get_Apostado();
+
+   //Se o bot já estiver apostado o valor da mesa, então ele dará check()
+   if(aposta == 0) return true;
+
+   //Em caso contrário verificaremos se ele ainda pode apostar nessa rodada
+   else if(this->get_Saldo() + this->get_Apostado() >= valorMesa)
+   {
+      int aposta_em_fichas = 0, ficha, quantidadeFicha;
+        
+      for (int i = 0; i < this->get_Fichas().size(); i++)
+      {
+         if(this->get_Fichas()[i].second > 0)
+         {
+            ficha = this->get_Fichas()[i].first;
+            quantidadeFicha = this->get_Fichas()[i].second;
+         }
+            
+         aposta_em_fichas += ficha * quantidadeFicha;
+         this->get_Fichas()[i].second -= quantidadeFicha;
+         // se a quantidade apostada em fichas ate então suprir o valor da aposta proposta, ele finaliza com um goto endloop
+         if(aposta_em_fichas >= aposta)
+         {
+            aposta_em_fichas -= aposta;
+            set_Apostado(this->get_Apostado() + aposta);
+            converte_Sobressalente(aposta_em_fichas);
+            return true;
+         }
+      }
+   }                 
+    
+   //Se a aposta for igual ao saldo do jogador, isso quer dizer que o jogador entrará em modo all in
+   else if(aposta == this->get_Saldo() && !(this->is_True_All_In()))
+   {
+      this->set_All_In(true);
+      this->aumenta_Saldo(0);
+      this->set_Apostado(get_Apostado() + aposta);
+      return true;
+   }
+      return false;
 }
+
 
